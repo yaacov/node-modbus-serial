2302500a0233f5a57fd1f6e5638d7fc6fbad3a7f
yaacov <yzamir@redhat.com>
2017-01-29 16:40:19 +0200

    remove debug listeners

--- index.js
+++ index.js
@@ -319,6 +319,7 @@ ModbusRTU.prototype.close = function (callback) {
     // close the serial port
     this._port.close(callback);
     this._port.removeAllListeners('data');
+    this._port.removeAllListeners('debug');
 };
 
 /**
