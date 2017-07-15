2504845bc80c66790ae1d45cd81db4bf9ffc8a4b
yaacov <kobi.zamir@gmail.com>
2017-07-15 23:47:48 +0300

    Clean server test

--- test/servers/servertcp.test.js
+++ test/servers/servertcp.test.js
@@ -3,13 +3,10 @@
 
 var expect = require("chai").expect;
 var net = require("net");
-/* var path = require("path");
-var childProcess = require("child_process"); */
 var TcpServer = require("./../../servers/servertcp");
 
 describe("Modbus TCP Server", function() {
     var serverTCP; // eslint-disable-line no-unused-vars
-    var sock;
 
     before(function() {
         var vector = {
@@ -22,17 +19,9 @@ describe("Modbus TCP Server", function() {
         serverTCP = new TcpServer(vector, { host: "0.0.0.0", port: 8512, debug: true, unitID: 1 });
     });
 
-    beforeEach(function() {
-        sock = new net.Socket();
-    });
-
-    afterEach(function() {
-        sock.end();
-    });
-
     describe("function code handler", function() {
         it("should receive a valid Modbus TCP message", function(done) {
-            const client = net.connect({ host: "0.0.0.0", port: 8512 }, () => {
+            const client = net.connect({ host: "0.0.0.0", port: 8512 }, function() {
                 // FC05 - force single coil, to on 0xff00
                 client.write(new Buffer("00010000000601050005ff00", "hex"));
             });
@@ -49,7 +38,7 @@ describe("Modbus TCP Server", function() {
 
     describe("modbus exception handler", function() {
         it("should receive a valid Modbus TCP message", function(done) {
-            const client = net.connect({ host: "0.0.0.0", port: 8512 }, () => {
+            const client = net.connect({ host: "0.0.0.0", port: 8512 }, function() {
                 // FC07 - unhandled function
                 client.write(new Buffer("000100000006010700000000", "hex"));
             });
@@ -64,19 +53,20 @@ describe("Modbus TCP Server", function() {
         // TODO: exceptions
     });
 
-    /* describe("socket connection error", function() {
+    describe("socket connection error", function() {
         it("should receive an error event on socket closed by client", function(done) {
+            var client = net.connect({ host: "0.0.0.0", port: 8512 }, function() {
+                client.destroy();
+
+                serverTCP.emit("socketError");
+            });
+
             serverTCP.on("socketError", function() {
                 // Error handled correctly
                 done();
             });
-
-            var child = childProcess.fork(path.join(__dirname, "../mocks/crashingClient.js"), ["0.0.0.0", 8512]);
-            child.on("message", function() {
-                child.kill();
-            });
         });
 
         // TODO: exceptions
-    }); */
+    });
 });
