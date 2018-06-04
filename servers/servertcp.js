ac109a37626de8548cbd269291737d8c9d9a4066
dushyant <dushyantb@winjit.com>
2018-06-04 18:13:57 +0530

    Fixes #130

--- servers/servertcp.js
+++ servers/servertcp.js
@@ -285,39 +285,81 @@ function _handleReadMultipleRegisters(requestBuffer, vector, unitID, callback) {
     var responseBuffer = Buffer.alloc(3 + length * 2 + 2);
     responseBuffer.writeUInt8(length * 2, 2);
 
-    // read registers
-    if (vector.getHoldingRegister) {
-        var callbackInvoked = false;
-        var cbCount = 0;
-        var buildCb = function(i) {
-            return function(err, value) {
-                if (err) {
-                    if (!callbackInvoked) {
-                        callbackInvoked = true;
-                        callback(err);
-                    }
-
-                    return;
-                }
-
-                cbCount = cbCount + 1;
-
-                responseBuffer.writeUInt16BE(value, 3 + i * 2);
-
-                if (cbCount === length && !callbackInvoked) {
-                    modbusSerialDebug({ action: "FC3 response", responseBuffer: responseBuffer });
-
+    var callbackInvoked = false;
+    var cbCount = 0;
+    var buildCb = function(i) {
+        return function(err, value) {
+            if (err) {
+                if (!callbackInvoked) {
                     callbackInvoked = true;
-                    callback(null, responseBuffer);
+                    callback(err);
                 }
-            };
-        };
 
-        if (length === 0)
-            callback({
-                modbusErrorCode: 0x02, // Illegal address
-                msg: "Invalid length"
-            });
+                return;
+            }
+
+            cbCount = cbCount + 1;
+
+            responseBuffer.writeUInt16BE(value, 3 + i * 2);
+
+            if (cbCount === length && !callbackInvoked) {
+                modbusSerialDebug({ action: "FC3 response", responseBuffer: responseBuffer });
+
+                callbackInvoked = true;
+                callback(null, responseBuffer);
+            }
+        };
+    };
+
+    if (length === 0)
+        callback({
+            modbusErrorCode: 0x02, // Illegal address
+            msg: "Invalid length"
+        });
+
+    // read registers
+    if (vector.getMultipleHoldingRegisters && length > 1) {
+
+        if(vector.getMultipleHoldingRegisters.length===4){
+            vector.getMultipleHoldingRegisters(address,length,unitID,function(err,values){
+                if(values.length!=length){
+                    var error=new Error("Requested address length and response length do not match");
+                    callback(error);
+                    throw error;
+                }else{
+                    for (var i = 0; i < length; i++) {
+                        var cb = buildCb(i);
+                        try {
+                            cb(err, values[i]);
+                        }
+                        catch(err) {
+                            cb(err);
+                        }
+                    }
+                }
+            })
+        }else{
+            var values=vector.getMultipleHoldingRegisters(address,length,unitID);
+            if(values.length!=length){
+                var error=new Error("Requested address length and response length do not match");
+                callback(error);
+                throw error;
+            }else{
+                for (var i = 0; i < length; i++) {
+                    var cb = buildCb(i);
+                    try {
+                        var promiseOrValue = values[i];
+                        _handlePromiseOrValue(promiseOrValue, cb);
+                    }
+                    catch(err) {
+                        cb(err);
+                    }
+                }
+            }
+        }
+
+    }
+    else if (vector.getHoldingRegister) {
 
         for (var i = 0; i < length; i++) {
             var cb = buildCb(i);
@@ -359,38 +401,79 @@ function _handleReadInputRegisters(requestBuffer, vector, unitID, callback) {
     var responseBuffer = Buffer.alloc(3 + length * 2 + 2);
     responseBuffer.writeUInt8(length * 2, 2);
 
-    if (vector.getInputRegister) {
-        var callbackInvoked = false;
-        var cbCount = 0;
-        var buildCb = function(i) {
-            return function(err, value) {
-                if (err) {
-                    if (!callbackInvoked) {
-                        callbackInvoked = true;
-                        callback(err);
-                    }
-
-                    return;
-                }
-
-                cbCount = cbCount + 1;
-
-                responseBuffer.writeUInt16BE(value, 3 + i * 2);
-
-                if (cbCount === length && !callbackInvoked) {
-                    modbusSerialDebug({ action: "FC4 response", responseBuffer: responseBuffer });
-
+    var callbackInvoked = false;
+    var cbCount = 0;
+    var buildCb = function(i) {
+        return function(err, value) {
+            if (err) {
+                if (!callbackInvoked) {
                     callbackInvoked = true;
-                    callback(null, responseBuffer);
+                    callback(err);
                 }
-            };
-        };
 
-        if (length === 0)
-            callback({
-                modbusErrorCode: 0x02, // Illegal address
-                msg: "Invalid length"
-            });
+                return;
+            }
+
+            cbCount = cbCount + 1;
+
+            responseBuffer.writeUInt16BE(value, 3 + i * 2);
+
+            if (cbCount === length && !callbackInvoked) {
+                modbusSerialDebug({ action: "FC4 response", responseBuffer: responseBuffer });
+
+                callbackInvoked = true;
+                callback(null, responseBuffer);
+            }
+        };
+    };
+
+    if (length === 0)
+        callback({
+            modbusErrorCode: 0x02, // Illegal address
+            msg: "Invalid length"
+        });
+    if (vector.getMultipleInputRegisters && length > 1) {
+
+        if(vector.getMultipleInputRegisters.length===4){
+            vector.getMultipleInputRegisters(address,length,unitID,function(err,values){
+                if(values.length!=length){
+                    var error=new Error("Requested address length and response length do not match");
+                    callback(error);
+                    throw error;
+                }else{
+                    for (var i = 0; i < length; i++) {
+                        var cb = buildCb(i);
+                        try {
+                            cb(err, values[i]);
+                        }
+                        catch(err) {
+                            cb(err);
+                        }
+                    }
+                }
+            })
+        }else{
+            var values=vector.getMultipleInputRegisters(address,length,unitID);
+            if(values.length!=length){
+                var error=new Error("Requested address length and response length do not match");
+                callback(error);
+                throw error;
+            }else{
+                for (var i = 0; i < length; i++) {
+                    var cb = buildCb(i);
+                    try {
+                        var promiseOrValue = values[i];
+                        _handlePromiseOrValue(promiseOrValue, cb);
+                    }
+                    catch(err) {
+                        cb(err);
+                    }
+                }
+            }
+        }
+
+    }
+    else if (vector.getInputRegister) {
 
         for (var i = 0; i < length; i++) {
             var cb = buildCb(i);
