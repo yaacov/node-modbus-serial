9a1d61d222ea7bed5f6e559c0984f18b019e5f38
cplepage <lepage.charlespilippe@gmail.com>
2024-04-04 11:16:01 -0400

    file

---
+++ ServerSerial.d.ts
@@ -0,0 +1,19 @@
+import * as events from 'events';
+import { OpenOptions } from "serialport"
+import { FCallback, IServiceVector } from './ServerTCP';
+
+export class ServerSerial extends events.EventEmitter {
+    constructor(vector: IServiceVector, options: IServerSerialOptions);
+    close(cb: FCallback): void;
+}
+
+type IServerSerialOptions = OpenOptions & {
+    debug?: boolean,
+    unitID?: number
+}
+
+export declare interface ServerSerial {
+    on(event: 'socketError', listener: FCallback): this;
+    on(event: 'error', listener: FCallback): this;
+    on(event: 'initialized', listener: FCallback): this;
+}
