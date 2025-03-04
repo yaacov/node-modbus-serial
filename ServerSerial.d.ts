f8f370efe4ff7a72aff5c72de66df78972247ac2
Chris Vaas <auspex@auspex.eu>
2025-03-04 13:39:55 +0100

    Update ServerSerial.d.ts (#585)

--- ServerSerial.d.ts
+++ ServerSerial.d.ts
@@ -1,11 +1,11 @@
 import * as events from 'events';
-import { SerialPortOpenOptions } from "serialport"
+import * as stream from 'stream';
+import { SerialPortOpenOptions } from 'serialport';
 import { FCallback, IServiceVector } from './ServerTCP';
 import { ErrorCallback } from '@serialport/stream';
-import ServerSerialPipeHandler from './servers/serverserial_pipe_handler';
 
 export class ServerSerial extends events.EventEmitter {
-    socks: Map<ServerSerialPipeHandler, 0>;
+    socks: Map<stream.Transform, 0>;
     constructor(vector: IServiceVector, options: IServerSerialOptions);
     close(cb: FCallback): void;
 }
